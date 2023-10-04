f1d97abaa0c054d4d257e8bf048322c23b17b696
Unstopabloo <pablooyarceramirez@gmail.com>
2023-10-04 00:19:23 -0300

    CORS solved

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,23 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ['*']
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:8888',
+  'http://localhost:5500',
+  'http://localhost:1234',
+  'http://localhost:3000',
+  'http://localhost:5137',
+  'http://127.0.0.1:5500',
+  'http://127.0.0.1:1234',
+  'http://127.0.0.1:3000',
+  'http://127.0.0.1:5137',
+  '*'
+]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      if (ACCEPTED_ORIGINS.includes(origin)) {
+      if (acceptedOrigins.includes('*') || acceptedOrigins.includes(origin)) {
         return callback(null, true)
       }
 
